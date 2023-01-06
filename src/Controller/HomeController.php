761ed2fd547d992d99c2c3a58636e0f0f6747e01
NicolasPgm <nicolas.paugam100@gmail.com>
2023-01-06 17:28:09 +0100

    affichage de trois biens aleatoirement sur la page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,16 +6,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CategorieRepository;
+use App\Entity\Bien;
+use App\Repository\BienRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(CategorieRepository $categorieRepository): Response
+    public function index(CategorieRepository $categorieRepository, BienRepository $bienRepository): Response
     {
+        #$biens = $categorieRepository->findAll();
+        # les trois lignes suivante serve à la génération de trois biens aléatoire
+        $biens = $bienRepository->findAll();
+        shuffle($biens);
+        $randomBiens = array_slice($biens, 0, 3);
+
         return $this->render('home/index.html.twig', [
             'current_menu' => 'home',
-            'categories' => $categorieRepository->findAll()
+            'categories' => $categorieRepository->findAll(),
+            #'biens' => $bienRepository->findAll()
+            'biens' => $randomBiens
 
+        ]);
+    }
+
+    #[Route('/{id}/show', name: 'app_home_show', methods: ['GET'])]
+    public function show(Bien $biens, CategorieRepository $rep): Response
+    {
+        $biens = $rep->findAll();
+        return $this->render('bien/parties/show.html.twig', [
+            'categories' => $rep->findAll(),
+            'biens' => $biens,
 
         ]);
     }
